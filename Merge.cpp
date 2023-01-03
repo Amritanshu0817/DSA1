35a9a80f5e06720b829f900ca3b1a00ec008841d
AMRITANSHU SHEKHAR <82936412+Amritanshu0817@users.noreply.github.com>
2023-01-03 16:42:38 +0530

    Update Merge.cpp

--- Merge.cpp
+++ Merge.cpp
@@ -11,7 +11,6 @@ void merge(int *arr, int s, int e) {
     int *first = new int[len1];
     int *second = new int[len2];
 
-    //copy values
     int mainArrayIndex = s;
     for(int i=0; i<len1; i++) {
         first[i] = arr[mainArrayIndex++];
@@ -21,8 +20,7 @@ void merge(int *arr, int s, int e) {
     for(int i=0; i<len2; i++) {
         second[i] = arr[mainArrayIndex++];
     }
-
-    //merge 2 sorted arrays     
+    
     int index1 = 0;
     int index2 = 0;
     mainArrayIndex = s;
@@ -50,21 +48,17 @@ void merge(int *arr, int s, int e) {
 }
 
 void mergeSort(int *arr, int s, int e) {
-
-    //base case
+    
     if(s >= e) {
         return;
     }
     
     int mid = (s+e)/2;
-
-    //left part sort karna h 
+    
     mergeSort(arr, s, mid);
     
-    //right part sort karna h 
     mergeSort(arr, mid+1, e);
 
-    //merge
     merge(arr, s, e);
 
 }
